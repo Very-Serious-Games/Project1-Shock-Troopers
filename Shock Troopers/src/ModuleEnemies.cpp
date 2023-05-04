9826a7d9479943ab994968804f4aea697dbb7e36
Apb8 <alexiapala8@gmail.com>
2023-05-04 20:38:39 +0200

    Update ModuleEnemies.cpp

--- Shock Troopers/src/ModuleEnemies.cpp
+++ Shock Troopers/src/ModuleEnemies.cpp
@@ -27,7 +27,7 @@ ModuleEnemies::~ModuleEnemies()
 
 bool ModuleEnemies::Start()
 {
-	texture = App->textures->Load("Assets/Sprites/enemies.png");
+	texture = App->textures->Load("Assets/Sprites/enemies_sprites-final.png");
 	enemyDestroyedFx = App->audio->LoadFx("Assets/Fx/explosion.wav");
 
 	return true;

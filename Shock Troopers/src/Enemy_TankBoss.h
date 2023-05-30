6e5219bd99fec0949798358ec32a7623c6aba8d2
mdoradom <mariodoradomartinez@gmail.com>
2023-05-30 11:32:19 +0200

    added tank boss files

---
+++ Shock Troopers/src/Enemy_TankBoss.h
@@ -0,0 +1,36 @@
+#ifndef __ENEMY_TANKBOSS_H__
+#define __ENEMY_TANKBOSS_H__
+
+#include "Enemy.h"
+#include "Path.h"
+#include "Module.h"
+
+class Enemy_TankBoss : public Enemy {
+public:
+	// Constructor (x y coordinates in the world)
+	// Creates animation and movement data and the collider
+	Enemy_TankBoss(int x, int y);
+
+	// The enemy is going to follow the different steps in the path
+	// Position will be updated depending on the speed defined at each step
+	void Update() override;
+
+	void deathAnimation() override;
+
+	void idleAnimation() override;
+
+	void spawnAnimation() override;
+
+	void StateMachine() override;
+
+	void Attack() override;
+
+private:
+	// The path that will define the position in the world
+	Path path;
+
+	SDL_Texture* textureTankBoss = nullptr;
+
+};
+
+#endif // __ENEMY_TANKBOSS_H__

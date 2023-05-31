378375b10ed6de385e21719e3a5ec2e7587f4615
mdoradom <mariodoradomartinez@gmail.com>
2023-05-31 11:22:05 +0200

    temp camp fix

--- Shock Troopers/src/SceneLevel1.cpp
+++ Shock Troopers/src/SceneLevel1.cpp
@@ -32,9 +32,11 @@ bool SceneLevel1::Start()
 	App->ui->Enable();
 
 	// Add colliders
+	/*
 	App->collisions->AddCollider({ 0, 1100, 486, 186 }, Collider::Type::STOP_CAM_ZONE);
 	App->collisions->AddCollider({ 838, 1100, 191, 102}, Collider::Type::STOP_CAM_ZONE);
 	App->collisions->AddCollider({ 1255, 3, 356, 217}, Collider::Type::STOP_CAM_ZONE);
+	*/
 
 	createMargenes();
 
